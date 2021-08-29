3d2711ea7cfca6ba5149c28063db1d79bd320880
Phantom <Phantom@DESKTOP-920PA8E>
2021-08-29 17:07:13 -0300

    added angular material and menu component init

--- front-end/src/app/modules/home/home.module.ts
+++ front-end/src/app/modules/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ItemListComponent } from './components/item-list/item-list.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     ItemListComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule,
